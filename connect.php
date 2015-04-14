8a5ee2174b9505ee8f6e8a4c7ba376ff884f1578
DominicMorquecho <dominic.morquecho@gmail.com>
2015-04-14 11:28:35 -0700

    Video 4 coded completed and commented

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo'); /*localhost is what you're connecting to, root is the username and password and todo is the database*/
+	if($mysqli->connect_error) { /*if theres a connection error than it will die and have this message*/
+		die('Connect Error (' . $mysqli->connect_errno . ')'
+			. $mysqli->connect_error);
+	}
+	else{ /*other wise echo that the connection was made*/
+		echo "Connection made";
+	}
+	$mysqli->close();
+?>
